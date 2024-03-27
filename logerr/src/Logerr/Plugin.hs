bc1c4ecb061442ffa7f716947ff9ec65501f9829
Piyush Garg <piyush.garg@juspay.in>
2024-03-27 18:44:11 +0530

    Resolve comments and cleanup

--- logerr/src/Logerr/Plugin.hs
+++ logerr/src/Logerr/Plugin.hs
@@ -79,47 +79,45 @@ import TcType
 import TysWiredIn
 import GHC.Hs.Lit (HsLit(..))
 
-logDebugInfo :: Bool
-logDebugInfo = True
-
-throwCompilationError :: Bool
-throwCompilationError = False
-
 plugin :: Plugin
 plugin = defaultPlugin {
-    typeCheckResultAction = logerr
+      typeCheckResultAction = logerr
     , pluginRecompile = purePlugin
     }
+
+logDebugInfo :: Bool
+logDebugInfo = False
 
 purePlugin :: [CommandLineOption] -> IO PluginRecompile
 purePlugin _ = return NoForceRecompile
 
 logerr :: [CommandLineOption] -> ModSummary -> TcGblEnv -> TcM TcGblEnv
 logerr opts modSummary tcEnv = do
-  let prefixPath = case opts of
-                        []    -> "test/output"
-                        local : _ -> local
-      moduleName' = moduleNameString $ moduleName $ ms_mod modSummary
-      modulePath = prefixPath <> ms_hspp_file modSummary
-  -- liftIO $ forkIO $ do
-      -- depsMapList <- mapM loopOverLHsBindLR $ bagToList $ tcg_binds tcEnv
-      -- let path = (intercalate "/" . reverse . tail . reverse . splitOn "/") modulePath
-      -- print ("generated dependancy for module: " <> moduleName' <> " at path: " <> path)
-      -- createDirectoryIfMissing True path
-      -- writeFile ((modulePath) <> ".json") (encodePretty $ concat depsMapList)
+  let
+    defaultSavePath =  ".juspay/tmp/logerr/"
+    defaultThrowCompilationError = True
+    defaultSaveToFile = False
+    (throwCompilationError, saveToFile, savePath) = 
+      case opts of
+        (x : y : z : _) -> (x == "true", y == "true", z)
+        (x : y : _)     -> (x == "true", y == "true", defaultSavePath)
+        (x : _)         -> (x == "true", defaultSaveToFile, defaultSavePath)
+        []              -> (defaultThrowCompilationError, defaultSaveToFile, defaultSavePath)
 
   errors <- concat <$> (mapM loopOverModBinds $ bagToList $ tcg_binds tcEnv)
-  -- if length errors > 0
-    -- then liftIO $ print ("More than 0 errors: " <> show errors)
-    -- else liftIO $ print "No errors"
+
   if throwCompilationError
     then addErrs $ map mkGhcCompileError errors
     else pure ()
-  addErrToFile modSummary errors  
+
+  if saveToFile
+    then addErrToFile modSummary savePath errors  
+    else pure ()
 
   return tcEnv
 
 --------------------------- Core Logic Hardcoded Data ---------------------------
+-- TODO: Take these from the configuration file
 badPracticeRules :: Rules
 badPracticeRules = [
     defaultRule
@@ -233,11 +231,6 @@ isBadFunAppHelper ap = do
   -- let (fnName, args) = maybe ("NA", []) (\(x, y) -> ((nameStableString . varName . unLoc) x, y)) $ res
   let (fnName, args) = maybe ("NA", []) (\(x, y) -> ((getOccString . varName . unLoc) x, y)) $ res
       rule = fromMaybe defaultRule $ (\x -> fnName == fn_name x) `find` badPracticeRules
-  -- if logDebugInfo && fnName /= "NA"
-  --   then liftIO $ do
-  --     print $ (fnName, map showS args)
-  --     print $ rule
-  --   else pure ()
   if rule == defaultRule
     then pure Nothing
   else if (arg_no rule) == 0 -- considering arg 0 as the case for blocking the whole function occurence
@@ -294,14 +287,6 @@ getFnNameWithAllArgs (L _ (HsApp _ funl funr)) = do
 getFnNameWithAllArgs (L loc ap@(HsWrap _ _ expr)) = do
   getFnNameWithAllArgs (L loc expr)
 getFnNameWithAllArgs _ = Nothing
-
--- TODO: Implement this
-isLogArgInFunArg :: LHsBindLR GhcTc GhcTc -> TcM Bool
-isLogArgInFunArg _ = pure False
-
--- TODO: Implement this function to update the list of functions which accept the string to log as function argt
-findAndUpdateFunctionsWithArgLogger :: LHsBindLR GhcTc GhcTc -> TcM [LHsBindLR GhcTc GhcTc]
-findAndUpdateFunctionsWithArgLogger _ = pure []
 
 --------------------------- Utils ---------------------------
 -- Check if HsExpr is Function Application
@@ -438,174 +423,10 @@ getBlockedFnsList arg rule =
       blockedFns = fns_blocked_in_arg rule
   in map getOccString $ filter (\x -> ((getOccString x) `elem` blockedFns) && (not . isSystemName . varName) x) vars
 
-addErrToFile :: ModSummary -> [CompileError] -> TcM ()
-addErrToFile modSummary errs = do
-  let path = "test/output/"
-      moduleName' = moduleNameString $ moduleName $ ms_mod modSummary
-      res = (encodePretty moduleName') <> (fromString ": ") <> encodePretty errs <> (fromString ",")
+addErrToFile :: ModSummary -> String -> [CompileError] -> TcM ()
+addErrToFile modSummary path errs = do
+  let moduleName' = moduleNameString $ moduleName $ ms_mod modSummary
+      -- res = (encodePretty moduleName') <> (fromString ": ") <> encodePretty errs <> (fromString ",")
+      res = encodePretty errs
   liftIO $ createDirectoryIfMissing True path
-  liftIO $ writeFile (path <> moduleName' <> "_compilationErrors.js") res
-
--- Fdep plugin -------------------------------------------------------------------
-
-transformFromNameStableString :: (Maybe String,Maybe String) -> Maybe FunctionInfo
-transformFromNameStableString (Just str,Just loc) =
-  let parts = filter (\x -> x /= "") $ splitOn ("$") str
-  in Just $ if length parts == 2 then  FunctionInfo "" (parts !! 0) (parts !! 1) loc else FunctionInfo (parts !! 0) (parts !! 1) (parts !! 2) loc
-
-loopOverLHsBindLR :: LHsBindLR GhcTc GhcTc -> IO [Function]
-loopOverLHsBindLR (L _ (FunBind _ id matches _ _)) = do
-  let funName = getOccString $ unLoc id
-      matchList = mg_alts matches
-  (list,funcs) <- foldM (\(x,y) xx -> do
-                                  (l,f) <- processMatch xx
-                                  pure $ (x <> l,y <> f)
-                        ) ([],[]) (unLoc matchList)
-  let listTransformed = map transformFromNameStableString $ nub $ list
-  pure [(Function funName listTransformed (nub funcs) (showSDocUnsafe $ ppr $ getLoc id))]
-loopOverLHsBindLR (L _ (PatBind _ _ pat_rhs _)) = do
-  let l = map transformFromNameStableString $ concatMap processGRHS $ grhssGRHSs pat_rhs
-  pure [(Function "" l [] "")]
-loopOverLHsBindLR (L _ VarBind {var_rhs = rhs}) = do
-  pure [(Function "" (map transformFromNameStableString $ processExpr rhs) [] "")]
-loopOverLHsBindLR (L _ AbsBinds {abs_binds = binds}) = do
-  list <- mapM loopOverLHsBindLR $ bagToList binds
-  pure (concat list)
-loopOverLHsBindLR (L _ (PatSynBind _ PSB {psb_def = def})) = do
-  let list = map transformFromNameStableString $ map (\(n,srcLoc) -> (Just $ traceShowId $ nameStableString n, srcLoc)) $ processPat def
-  pure [(Function "" list [] "")]
-loopOverLHsBindLR (L _ (PatSynBind _ (XPatSynBind _))) = do
-  pure []
-loopOverLHsBindLR (L _ (XHsBindsLR _)) = do
-  pure []
-
-processMatch :: LMatch GhcTc (LHsExpr GhcTc) -> IO ([(Maybe String,Maybe String)],[Function])
-processMatch (L _ match) = do
-  whereClause <- processHsLocalBinds $ unLoc $ grhssLocalBinds (m_grhss match)
-  pure $ (concatMap processGRHS $ grhssGRHSs (m_grhss match),whereClause)
-
-processGRHS :: LGRHS GhcTc (LHsExpr GhcTc) -> [(Maybe String,Maybe String)]
-processGRHS (L _ (GRHS _ _ body)) = processExpr body
-processGRHS _ = []
-
-processHsLocalBinds :: HsLocalBindsLR GhcTc GhcTc -> IO [Function]
-processHsLocalBinds (HsValBinds _ (ValBinds _ x y)) = do
-  res <- mapM loopOverLHsBindLR $ bagToList $ x
-  pure $ concat res
-processHsLocalBinds (HsValBinds _ (XValBindsLR (NValBinds x y))) = do
-  res <- foldM (\acc (recFlag,binds) -> do 
-                  funcs <- mapM loopOverLHsBindLR $ bagToList binds
-                  pure (acc <> funcs)
-                ) [] x
-  pure $ concat res
-processHsLocalBinds x =
-  pure []
-
-processExpr :: LHsExpr GhcTc -> [(Maybe String,Maybe String)]
-processExpr x@(L _ (HsVar _ (L _ var))) =
-  let name = nameStableString $ varName var
-  in [(Just name,Just $ showSDocUnsafe $ ppr $ getLoc $ x)]
-processExpr (L _ (HsUnboundVar _ _)) = []
-processExpr (L _ (HsApp _ funl funr)) =
-  processExpr funl <> processExpr funr
-processExpr (L _ (OpApp _ funl funm funr)) =
-  processExpr funl <> processExpr funm <> processExpr funr
-processExpr (L _ (NegApp _ funl _)) =
-  processExpr funl
-processExpr (L _ (HsTick _ _ fun)) =
-  processExpr fun
-processExpr (L _ (HsStatic _ fun)) =
-  processExpr fun
-processExpr (L _ (HsWrap _ _ fun)) =
-  processExpr (noLoc fun)
-processExpr (L _ (HsBinTick _ _ _ fun)) =
-  processExpr fun
-processExpr (L _ (ExplicitList _ _ funList)) =
-  concatMap processExpr funList
-processExpr (L _ (HsTickPragma _ _ _ _ fun)) =
-  processExpr fun
-processExpr (L _ (HsSCC _ _ _ fun)) =
-  processExpr fun
-processExpr (L _ (HsCoreAnn _ _ _ fun)) =
-  processExpr fun
-processExpr (L _ (ExprWithTySig _ fun _)) =
-  processExpr fun
-processExpr (L _ (HsDo _ _ exprLStmt)) =
-  let stmts = exprLStmt ^? biplateRef :: [LHsExpr GhcTc]
-   in nub $ concatMap processExpr stmts
-processExpr (L _ (HsLet _ exprLStmt func)) =
-  let stmts = exprLStmt ^? biplateRef :: [LHsExpr GhcTc]
-   in processExpr func <> nub (concatMap processExpr stmts)
-processExpr (L _ (HsMultiIf _ exprLStmt)) =
-  let stmts = exprLStmt ^? biplateRef :: [LHsExpr GhcTc]
-   in nub (concatMap processExpr stmts)
-processExpr (L _ (HsIf _ exprLStmt funl funm funr)) =
-  let stmts = (exprLStmt ^? biplateRef :: [LHsExpr GhcTc])
-   in nub (concatMap processExpr $ [funl, funm, funr] <> stmts)
-processExpr (L _ (HsCase _ funl exprLStmt)) =
-  let stmts = (exprLStmt ^? biplateRef :: [LHsExpr GhcTc])
-   in nub (concatMap processExpr $ [funl] <> stmts)
-processExpr (L _ (ExplicitSum _ _ _ fun)) = processExpr fun
-processExpr (L _ (SectionR _ funl funr)) = processExpr funl <> processExpr funr
-processExpr (L _ (ExplicitTuple _ exprLStmt _)) =
-  let stmts = (exprLStmt ^? biplateRef :: [LHsExpr GhcTc])
-   in nub (concatMap processExpr stmts)
-processExpr (L _ (RecordUpd _ rupd_expr rupd_flds)) = processExpr rupd_expr <> concatMap extractLHsRecUpdField rupd_flds
-processExpr (L _ (HsPar _ fun)) = processExpr fun
-processExpr (L _ (HsAppType _ fun _)) = processExpr fun
-processExpr (L _ (HsLamCase _ exprLStmt)) =
-  let stmts = (exprLStmt ^? biplateRef :: [LHsExpr GhcTc])
-   in nub (concatMap processExpr stmts)
-processExpr (L _ (HsLam _ exprLStmt)) =
-  let stmts = (exprLStmt ^? biplateRef :: [LHsExpr GhcTc])
-   in nub (concatMap processExpr stmts)
-processExpr (L _ (HsLit _ exprLStmt)) =
-  let stmts = (exprLStmt ^? biplateRef :: [LHsExpr GhcTc])
-   in nub (concatMap processExpr stmts)
-processExpr (L _ (HsOverLit _ exprLStmt)) =
-  let stmts = (exprLStmt ^? biplateRef :: [LHsExpr GhcTc])
-   in nub (concatMap processExpr stmts)
-processExpr (L _ (HsRecFld _ exprLStmt)) =
-  let stmts = (exprLStmt ^? biplateRef :: [LHsExpr GhcTc])
-   in nub (concatMap processExpr stmts)
-processExpr (L _ (HsSpliceE exprLStmtL exprLStmtR)) =
-  let stmtsL = (exprLStmtL ^? biplateRef :: [LHsExpr GhcTc])
-      stmtsR = (exprLStmtR ^? biplateRef :: [LHsExpr GhcTc])
-   in nub (concatMap processExpr (stmtsL <> stmtsR))
-processExpr (L _ (ArithSeq _ (Just exprLStmtL) exprLStmtR)) =
-  let stmtsL = (exprLStmtL ^? biplateRef :: [LHsExpr GhcTc])
-      stmtsR = (exprLStmtR ^? biplateRef :: [LHsExpr GhcTc])
-   in nub (concatMap processExpr (stmtsL <> stmtsR))
-processExpr (L _ (ArithSeq _ Nothing exprLStmtR)) =
-  let stmtsR = (exprLStmtR ^? biplateRef :: [LHsExpr GhcTc])
-   in nub (concatMap processExpr stmtsR)
-processExpr (L _ (HsRnBracketOut _ exprLStmtL exprLStmtR)) =
-  let stmtsL = (exprLStmtL ^? biplateRef :: [LHsExpr GhcTc])
-      stmtsR = (exprLStmtR ^? biplateRef :: [LHsExpr GhcTc])
-   in nub (concatMap processExpr (stmtsL <> stmtsR))
-processExpr (L _ (HsTcBracketOut _ exprLStmtL exprLStmtR)) =
-  let stmtsL = (exprLStmtL ^? biplateRef :: [LHsExpr GhcTc])
-      stmtsR = (exprLStmtR ^? biplateRef :: [LHsExpr GhcTc])
-   in nub (concatMap processExpr (stmtsL <> stmtsR))
--- HsIPVar (XIPVar p) HsIPName
--- HsOverLabel (XOverLabel p) (Maybe (IdP p)) FastString
--- HsConLikeOut (XConLikeOut p) ConLike
-processExpr _ = []
-
-extractLHsRecUpdField :: GenLocated l (HsRecField' id (LHsExpr GhcTc)) -> [(Maybe String,Maybe String)]
-extractLHsRecUpdField (L _ (HsRecField _ fun _)) = processExpr fun
-
-processPat :: LPat GhcTc -> [(Name,Maybe String)]
-processPat (L _ pat) = case pat of
-  ConPatIn _ details -> processDetails details
-  VarPat _ x@(L _ var) -> [(varName var,Just $ showSDocUnsafe $ ppr $ getLoc $ x)]
-  ParPat _ pat' -> processPat pat'
-  _ -> []
-
-processDetails :: HsConPatDetails GhcTc -> [(Name,Maybe String)]
-processDetails (PrefixCon args) = concatMap processPat args
-processDetails (InfixCon arg1 arg2) = processPat arg1 <> processPat arg2
-processDetails (RecCon rec) = concatMap processPatField (rec_flds rec)
-
-processPatField :: LHsRecField GhcTc (LPat GhcTc) -> [(Name,Maybe String)]
-processPatField (L _ HsRecField {hsRecFieldArg = arg}) = processPat arg
+  liftIO $ writeFile (path <> moduleName' <> "_compilationErrors.json") res
