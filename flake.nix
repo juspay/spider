f86d2d77d709590a81e0bfb34a14e3dab925445e
eswar2001 <eswar.tadiparth@juspay.in>
2024-07-19 13:26:18 +0530

    moving to ghc928

--- flake.nix
+++ flake.nix
@@ -1,19 +1,21 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/43e3b6af08f29c4447a6073e3d5b86a4f45dd420";
+    nixpkgs.url = "github:nixos/nixpkgs/75a52265bda7fd25e06e3a67dee3f0354e73243c";
     systems.url = "github:nix-systems/default";
     flake-parts.url = "github:hercules-ci/flake-parts";
     haskell-flake.url = "github:srid/haskell-flake";
     classyplate.flake = false;
-    classyplate.url = "github:Chaitanya-nair/classyplate/46f5e0e7073e1d047f70473bf3c75366a613bfeb";
     references.flake = true;
-    references.url = "github:eswar2001/references/35912f3cc72b67fa63a8d59d634401b79796469e";
     ghc-hasfield-plugin.flake = false;
-    large-records.flake = false;
-    ghc-hasfield-plugin.url = "github:juspay/ghc-hasfield-plugin/d82ac5a6c0ad643eebe2b9b32c91f6523d3f30dc";
-    large-records.url = "github:eswar2001/large-records/e393f4501d76a98b4482b0a5b35d120ae70e5dd3";
-    record-dot-preprocessor.url = "github:ndmitchell/record-dot-preprocessor/99452d27f35ea1ff677be9af570d834e8fab4caf";
-    record-dot-preprocessor.flake = false;
+    classyplate.url = "github:iamanandsingh/classyplate/2c869097df8fdbeb1da23842bf5f52366daf3893";
+    references.url = "github:iamanandsingh/references/8366257fbeb4f63e1ec0347c9fe5f203ba6fdebc";
+    large-records.url = "github:well-typed/large-records";
+    # references.url = "github:eswar2001/references/35912f3cc72b67fa63a8d59d634401b79796469e";
+    # classyplate.url = "github:Chaitanya-nair/classyplate/46f5e0e7073e1d047f70473bf3c75366a613bfeb";
+    ghc-hasfield-plugin.url = "github:eswar2001/ghc-hasfield-plugin/c932ebc0d7e824129bb70c8a078f3c68feed85c9";
+    # large-records.url = "github:eswar2001/large-records/e393f4501d76a98b4482b0a5b35d120ae70e5dd3";
+    # record-dot-preprocessor.url = "github:ndmitchell/record-dot-preprocessor/99452d27f35ea1ff677be9af570d834e8fab4caf";
+    # record-dot-preprocessor.flake = false;
   };
   outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
@@ -36,7 +38,8 @@
           # (defined by `defaults.packages` option).
           #
           projectFlakeName = "spider";
-          basePackages = pkgs.haskell.packages.ghc8107;
+          # basePackages = pkgs.haskell.packages.ghc8107;
+          basePackages = pkgs.haskell.packages.ghc92;
           imports = [
             inputs.references.haskellFlakeProjectModules.output
           ];
@@ -46,9 +49,12 @@
             large-records.source = inputs.large-records + /large-records;
             large-generics.source = inputs.large-records + /large-generics;
             large-anon.source = inputs.large-records + /large-anon;
-            ghc-tcplugin-api.source = "0.7.1.0";
             typelet.source = inputs.large-records + /typelet;
-            record-dot-preprocessor.source = inputs.record-dot-preprocessor;
+            # text.source = "2.0";
+            # Cabal-syntax.source = "3.8.1.0";
+            # fourmolu.source = "3.8.1.0";
+            # record-dot-preprocessor.source = inputs.record-dot-preprocessor;
+            # record-dot-preprocessor.source = "0.2.14";
           };
           settings = {
             #  aeson = {
@@ -58,7 +64,13 @@
             #    haddock = false;
             #    broken = false;
             #  };
-              sheriff.check = false;
+            # primitive-checked = {
+            #     broken = false;
+            #     jailbreak = true;
+            # };
+            ghc-hasfield-plugin.check = false;
+            ghc-hasfield-plugin.jailbreak = true;
+            sheriff.check = false;
           };
 
           devShell = {
