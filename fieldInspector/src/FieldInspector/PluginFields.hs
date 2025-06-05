f7a32f2dd941e6747e89806bd569f89172e6fa5a
eswar2001 <eswar.tadiparth@juspay.in>
2025-06-05 19:54:15 +0530

    added fieldInspector type updates

--- fieldInspector/src/FieldInspector/PluginFields.hs
+++ fieldInspector/src/FieldInspector/PluginFields.hs
@@ -244,9 +244,11 @@ import qualified Data.ByteString.Lazy as BL
 import qualified Data.Aeson as A
 import qualified Network.WebSockets as WS
 import Network.Socket (withSocketsDo)
-import Text.Read (readMaybe)
 import GHC.IO (unsafePerformIO)
-
+import qualified Network.Socket as NS
+import qualified Network.Socket.ByteString as NSB
+import System.Directory (createDirectoryIfMissing)
+import Data.Hashable (hash)
 
 
 plugin :: Plugin
@@ -266,32 +268,44 @@ removeIfExists fileName = removeFile fileName `catch` handleExists
         | isDoesNotExistError e = return ()
         | otherwise = throwIO e
 
-websocketPort :: Maybe Int
-websocketPort = maybe Nothing (readMaybe) $ unsafePerformIO $ lookupEnv "SERVER_PORT"
+fdepSocketPath :: Maybe FilePath
+fdepSocketPath = unsafePerformIO $ lookupEnv "FDEP_SOCKET_PATH"
 
-websocketHost :: Maybe String
-websocketHost = unsafePerformIO $ lookupEnv "SERVER_HOST"
+connectToUnixSocket :: FilePath -> IO NS.Socket
+connectToUnixSocket socketPath = do
+    sock <- NS.socket NS.AF_UNIX NS.Stream NS.defaultProtocol
+    NS.connect sock (NS.SockAddrUnix socketPath)
+    return sock
 
-
-sendFileToWebSocketServer :: CliOptions -> Text -> Text -> IO ()
+sendFileToWebSocketServer :: CliOptions -> Text -> _ -> IO ()
 sendFileToWebSocketServer cliOptions path data_ =
-    withSocketsDo $ do
-        eres <- try $
-            WS.runClient
-                (fromMaybe (host cliOptions) websocketHost)
-                (fromMaybe (port cliOptions) websocketPort)
-                (T.unpack path)
-                (\conn -> do
-                    res <- try $ WS.sendTextData conn data_
-                    case res of
-                        Left (err :: SomeException) ->
-                            when (log cliOptions) $ print err
-                        Right _ -> pure ()
-                )
-        case eres of
-            Left (err :: SomeException) ->
-                when (log cliOptions) $ print err
-            Right _ -> pure ()
+    -- Use the Unix Domain Socket implementation
+    sendViaUnixSocket cliOptions path data_
+
+sendViaUnixSocket :: CliOptions -> Text -> Text -> IO ()
+sendViaUnixSocket cliOptions path data_ = do
+    -- Create message with path as header for routing
+    let message = encodeUtf8 $ path <> "****" <> data_
+    
+    -- Get socket path from environment or config
+    let socketPathToUse = fromMaybe (FieldInspector.Types.path cliOptions) fdepSocketPath
+    
+    -- Try to send data
+    res <- try $ do
+        sock <- connectToUnixSocket socketPathToUse
+        NSB.sendAll sock (message)
+        NS.close sock
+    
+    case res of
+        Left (err :: SomeException) -> do
+            appendFile "error.log" ((T.unpack path) <> "," <> (T.unpack data_) <> "\n")
+            let errorDir = "fdep_recovery"
+            createDirectoryIfMissing True errorDir
+            let timestamp = show $ hash $ T.unpack path
+            appendFile (errorDir <> "/" <> timestamp <> ".json") (T.unpack data_ <> "\n")
+    
+        Right _ -> 
+            print $ "Successfully sent data to " <> path
 
 collectTypesTC :: [CommandLineOption] -> ModSummary -> TcGblEnv -> TcM TcGblEnv
 collectTypesTC opts modSummary tcEnv = do
